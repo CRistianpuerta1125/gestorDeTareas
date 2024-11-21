a1eed24753ff08554bd4ad22639c19194507a63f
DamianCalderon <crisdam1840@gmail.com>
2024-11-21 11:33:47 -0500

    Remove unused navigation bar from App component

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -10,22 +10,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/tasks">Mi App de Tareas</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="/tasks">Ver tareas</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
+        
         <Routes>
           <Route path="/" element={<Navigate to="/register" replace />} />
 
